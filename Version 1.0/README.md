3a46e1c413479cb4802e635bbb58ab9d77a915ee
Anonym2546 <johannes.25406@gmail.com>
2023-01-22 11:30:57 +0100

    ⠀

---
+++ Version 1.0/README.md
@@ -0,0 +1,17 @@
+# Contact:
+- https://discord.gg/WahFF8jXW5 
+
+# Requirements:
+- Python 3
+- Pip
+
+# Run:
+- Move the Un-Zipped Folder to your Favourite location
+- Double Click Start.bat
+
+# How To Use:
+- After the running the start.bat you can select the Game you want to play
+- Click on the Version to download and start a game
+
+# Coming Soon (Version 1.1):
+- Option to see the README.md of every game/version
--- Version 1.0/files/Start.bat
+++ Version 1.0/files/Start.bat
@@ -1,9 +1,30 @@
 echo off
-title JJSS Launcher Console
+cls
+if EXIST status.txt (
+goto start
+)
+echo **NOTE**
+echo On the first launch of the game, all required additional python modules have to be installed
+echo Depending on the already installed modules, this could take a while
+echo performing check for required modules
+for /F "delims=;" %%i in (requirements.txt) do (
+python -c "import pkgutil; import os; os.system('echo %%i: OK' if pkgutil.find_loader('%%i') else 'echo %%i: MISSING')"
+)
+echo installing required modules...
+for /F "delims=;" %%i in (requirements.txt) do (
+pip -q -q -q install %%i
+echo installed %%i
+)
+echo Installation successfull
+echo OK > status.txt
+echo proceeding to start game
+:start
+echo off
+title JJSS Launcher
 cls
 echo **INFO**
 echo This Screen has no meaning, just minimize it
-echo Running JJSS-Launcherv.1.0
+echo Running JJSS Launcher v.1.0
 python jjss_launcher.py
 cls
 echo Process terminated
